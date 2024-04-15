37cf8249f1fa6d0f7861df93177cf20e64d9b75b
Lisower <144545336+Lisower@users.noreply.github.com>
2024-04-15 14:24:18 +0300

    Update form.php

--- form.php
+++ form.php
@@ -23,10 +23,80 @@ if (!empty($messages)) {
 // и задавая начальные значения элементов ранее сохраненными.
 ?>
 
-    <form action="" method="POST">
-      <input name="fio" <?php if ($errors['fio']) {print 'class="error"';} ?> value="<?php print $values['fio']; ?>" />
-      <input type=date name="date" <?php if ($errors['date']) {print 'class="error"';} ?> value="<?php print $values['date']; ?>" />
-      <input type="submit" value="ok" />
-    </form>
+    <button id="Button">Открыть форму</button>
+
+        <div id="Popup" class="Popup">
+            <form id="Form" action="" method="POST">
+                <h1>Форма</h1>
+
+                <label>
+                    ФИО<br>
+                    <input name="FIO"
+                    placeholder="Введите Ваше ФИО"
+                    required>
+                </label><br>
+
+                <label>
+                Номер телефона<br>
+                <input name="phone_number"
+                    type="tel"
+                    placeholder="Введите Ваш номер телефона"
+                    required>
+                </label><br>
+
+                <label>
+                Почта e-mail<br>
+                <input name="e_mail"
+                    type="email"
+                    placeholder="Введите Вашу почту"
+                    required>
+                </label><br>
+
+                <label>
+                    Дата рождения<br>
+                    <input name="birthday"
+                    type="date"
+                    required>
+                </label><br>
+                
+                Пол<br>
+                <label><input type="radio"
+                name="sex" value="М" checked="checked">
+                М</label>
+                <label><input type="radio"
+                name="sex" value="Ж">
+                Ж</label><br>
+
+                <label>
+                    Любимый язык программирования<br>
+                    <select name="favourite_languages[]" required
+                        multiple="multiple">
+                        <option value="1">Pascal</option>
+                        <option value="2">C</option>
+                        <option value="3">C++</option>
+                        <option value="4">JavaScript</option>
+                        <option value="5">PHP</option>
+                        <option value="6">Python</option>
+                        <option value="7">Java</option>
+                        <option value="8">Haskel</option>
+                        <option value="9">Clojure</option>
+                        <option value="10">Prolog</option>
+                        <option value="11">Scala</option>
+                    </select>
+                </label><br>
+
+                <label>
+                    Биография<br>
+                    <textarea name="biography" placeholder="Напишите Вашу биографию"></textarea>
+                </label><br>
+
+                <label><input type="checkbox"
+                    name="check" required>
+                    С контрактом ознакомлен
+                </label><br>
+
+                <input type="submit" value="Сохранить">
+            </form>
+        </div>
   </body>
 </html>
