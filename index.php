ffbb2890a64a8f6902a64f2cf2a2531ec5c09cd3
Lisower <144545336+Lisower@users.noreply.github.com>
2024-04-22 14:37:20 +0300

    Update index.php

--- index.php
+++ index.php
@@ -33,11 +33,19 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
   $errors['phone_number_empty'] = !empty($_COOKIE['phone_number_empty']);
   $errors['phone_number_error'] = !empty($_COOKIE['phone_number_error']);
   
-  $errors['e_mail'] = !empty($_COOKIE['e_mail']);
-  $errors['birthday'] = !empty($_COOKIE['birthday']);
-  $errors['sex'] = !empty($_COOKIE['sex']);
+  $errors['e_mail_empty'] = !empty($_COOKIE['e_mail_empty']);
+  $errors['e_mail_error'] = !empty($_COOKIE['e_mail_error']);
+  
+  $errors['birthday_empty'] = !empty($_COOKIE['birthday_empty']);
+  $errors['birthday_error'] = !empty($_COOKIE['birthday_error']);
+  
+  $errors['sex_empty'] = !empty($_COOKIE['sex_empty']);
+  $errors['sex_error'] = !empty($_COOKIE['sex_error']);
+  
   $errors['favourite_languages'] = !empty($_COOKIE['favourite_languages']);
+  
   $errors['biography'] = !empty($_COOKIE['biography']);
+  
   $errors['check'] = !empty($_COOKIE['check']);
 
   if ($errors['FIO_empty']) {
@@ -45,7 +53,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     setcookie('FIO_value', '', 100000);
     $messages[] = '<div class="error">Заполните имя!</div>';
   }
-  if ($errors['FIO_error']) {
+  if ($errors['FIO_error'] $$ !$errors['FIO_empty']) {
     setcookie('FIO_error', '', 100000);
     setcookie('FIO_value', '', 100000);
     $messages[] = '<div class="error">Недопустимое имя! Допустимые символы: буквы английского и русского алфавитов</div>';
@@ -56,16 +64,28 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     setcookie('phone_number_value', '', 100000);
     $messages[] = '<div class="error">Введите номер телефона!</div>';
   }
-   if ($errors['phone_number_error']) {
+   if ($errors['phone_number_error'] && !$errors['phone_number_empty']) {
     setcookie('phone_number_error', '', 100000);
     setcookie('phone_number_value', '', 100000);
     $messages[] = '<div class="error">Недопустимый номер телефона! Допустимые символы: цифры 0-9</div>';
+  }
+  
+  if ($errors['e_mail_empty']) {
+    setcookie('e_mail_empty', '', 100000);
+    setcookie('e_mail_value', '', 100000);
+    $messages[] = '<div class="error">Введите почту!</div>';
+  }
+  if ($errors['e_mail_error'] && !$errors['e_mail_empty']) {
+    setcookie('e_mail_error', '', 100000);
+    setcookie('e_mail_value', '', 100000);
+    $messages[] = '<div class="error">Недопустимая почта! Формат почты: example@example.com</div>';
   }
 
   // Складываем предыдущие значения полей в массив, если есть.
   $values = array();
   $values['FIO'] = empty($_COOKIE['FIO_value']) ? '' : $_COOKIE['FIO_value'];
   $values['phone_number'] = empty($_COOKIE['phone_number_value']) ? '' : $_COOKIE['phone_number_value'];
+  $values['e_mail'] = empty($_COOKIE['e_mail_value']) ? '' : $_COOKIE['e_mail_value'];
   // TODO: аналогично все поля.
 
   // Включаем содержимое файла form.php.
@@ -77,6 +97,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 else {
   // Проверяем ошибки.
   $errors = FALSE;
+  
   if (empty($_POST['FIO'])) {
     setcookie('FIO_empty', '1', time() + 24 * 60 * 60);
     $errors = TRUE;
@@ -85,6 +106,7 @@ else {
     setcookie('FIO_error', '1', time() + 24 * 60 * 60);
     $errors = TRUE;
   }
+  
   if (empty($_POST['phone_number'])) {
     setcookie('phone_number_empty', '1', time() + 24 * 60 * 60);
     $errors = TRUE;
@@ -93,10 +115,19 @@ else {
     setcookie('phone_number_error', '1', time() + 24 * 60 * 60);
     $errors = TRUE;
   }
-  
+
+  if (empty($_POST['e_mail'])) {
+    setcookie('e_mail_empty', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  }
+  if (!preg_match('/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]$/', $_POST['e_mail'])) {
+    setcookie('e_mail_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  }
   // Сохраняем ранее введенное в форму значение на месяц.
   setcookie('FIO_value', $_POST['FIO'], time() + 30 * 24 * 60 * 60);
   setcookie('phone_number_value', $_POST['phone_number'], time() + 30 * 24 * 60 * 60);
+  setcookie('e_mail_value', $_POST['e_mail'], time() + 30 * 24 * 60 * 60);
 
 // *************
 // TODO: тут необходимо проверить правильность заполнения всех остальных полей.
@@ -115,6 +146,9 @@ else {
     
     setcookie('phone_number_empty', '', 100000);
     setcookie('phone_number_error', '', 100000);
+
+    setcookie('e_mail_empty', '', 100000);
+    setcookie('e_mail_error', '', 100000);
     // TODO: тут необходимо удалить остальные Cookies.
   }
 
