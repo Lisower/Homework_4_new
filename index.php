e71ab294d6c406028b929206013beb7c085c23c7
Lisower <144545336+Lisower@users.noreply.github.com>
2024-04-24 00:07:48 +0300

    Update index.php

--- index.php
+++ index.php
@@ -217,7 +217,7 @@ else {
 
   }
 
-  include(.gitignore)
+  include('.gitignore')
   $db = new PDO('mysql:host=localhost;dbname=u67447', $user, $pass,
     [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
   
